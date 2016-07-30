74a59b728b104d705b0095e12f0a57d5b136adbc
Dipeshkumar Patel <drpatel1111@yahoo.com>
2016-07-30 19:36:29 -0400

    update server

--- www/config.js
+++ www/config.js
@@ -2,7 +2,8 @@ var config = {
     //servicerooturl:"http://192.168.137.1/kbs/",
     //servicerooturl: "http://localhost:8080/htdocs/kbs/",
     //servicerooturl:"http://live.issousa.org/kirtanbhakti/",
-	servicerooturl:"http://live.issousa.org/kirtanbhakti/",
+	//servicerooturl:"http://live.issousa.org/kirtanbhakti/",
+	servicerooturl:"http://issousa.co/kirtanbhakti/",
     fbAppId: '731387763597618',
     fbMessage: 'Kirtan Bhakti',
     AppInfo: {
